8f103ee2a9009dcffab9ea5bf5a790f0c53c4680
Gako3113 <kougagaga@gmail.com>
2022-04-07 19:18:18 +0900

    [fix] component

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,17 +1,21 @@
-import React from "react"
+{/* componentの定義 */}
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'p', null, 'Hello world, it is ', now.toString()
-    ),
-    React.createElement(
-      'p', null, a, ' plus ', b, ' is ', a + b
-    )
+  return (
+    <div>
+      <h1>Greetings</h1>
+      {/* component 何度も使える */}
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
   )
 }
 
